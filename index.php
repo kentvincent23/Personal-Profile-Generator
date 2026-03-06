82f63541f1aa2cc6651f907468b49e2f7f5fe2f6
kentvincent23 <k.agot.145976.tc@umindanao.edu.ph>
2026-03-06 15:12:54 +0800

    TIWASA ANG PFP PART
    PLEASE

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+    <h1> Kent's Profile Generator!</h1>
     <form action="profile.php" method="post" enctype="multipart/form-data">
         <label for="fname">Full Name:</label>
         <input type="text" id="fname" name="fname"> <br><br>
@@ -17,27 +18,31 @@
         <input type="email" id="mail" name="mail"> <br><br>
         
         <label for="gender">Gender:</label>
-        <input type="radio" id="gender" name="gender" value="Male">
+        <input type="radio" id="Male" name="gender" value="Male">
         <label for="male"> Male </label>
-        <input type="radio" id="gender" name="gender" value="Female">
+        <input type="radio" id="Female" name="gender" value="Female">
         <label for="female"> Female </label> <br>
 
         <h3>Hobbies: </h3>
-        <input type="checkbox" id="hobbies1" name="hobbies1" value="gaming">
+        <input type="checkbox" id="gaming" name="hobbies[]" value="Gaming">
         <label for="gaming">Gaming</label>
-        <input type="checkbox" id="hobbies2" name="hobbies2" value="reading">
+        <input type="checkbox" id="reading" name="hobbies[]" value="Reading">
         <label for="reading">Reading</label>
-        <input type="checkbox" id="hobbies3" name="hobbies3" value="hiking">
+        <input type="checkbox" id="hiking" name="hobbies[]" value="Hiking">
         <label for="hiking">Hiking</label>
-        <input type="checkbox" id="hobbies4" name="hobbies4" value="singing">
-        <label for="singing">Singing</label>
-        <input type="checkbox" id="hobbies5" name="hobbies5" value="dancing">
-        <label for="dancing">Dancing</label>
+        <input type="checkbox" id="singing" name="hobbies[]" value="Singing">
+        <label for="singing">Singing</label> 
+        <input type="checkbox" id="dancing" name="hobbies[]" value="Dancing">
+        <label for="dancing">Dancing</label> <br>
+
+
+        <label for="bio"><h3>Short biography</h3></label>
+        <textarea id="bio" name="bio" rows="4" cols="50"> Add your biography here! (erase this text)</textarea> 
 
         <h3>Profile Picture: </h3>
         <label for="ppf">Upload a profile picture:</label>
-        <input type="file" id="ppf" name="ppf" accept="image/png, image/jpeg">
-        <input type="submit" value="Upload">
+        <input type="file" id="ppf" name="ppf"> <br>
+        <input type="submit" value="Upload" name="submit">
         </form>
 
 </body>
--- profile.php
+++ profile.php
@@ -1,4 +1,47 @@
+<?php 
+session_start();    
+?>
+
 <!DOCTYPE html>
 <html lang = 'en'>
-<title>Profile Generator Website</title>
-<body> Hello </body>
+<title>Profile Generation</title>
+<head> 
+    <meta charset = 'UTF-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <h1> - - PROFILE - - </h1>
+
+Name: <?php echo $_POST["fname"]; ?> <br>
+Age: <?php echo $_POST["age"]; ?> <br>
+Course: <?php echo $_POST["course"]; ?> <br>
+Mail: <?php echo $_POST["mail"]; ?> <br>
+Biography: <?php echo $_POST["bio"]; ?> <br>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['gender'])) {
+        $gender = $_POST['gender'];
+        echo "Your gender is: " . htmlspecialchars($gender) . "<br>";
+    } else {
+        echo "No gender was selected.<br>";
+    }
+if (isset($_POST['hobbies']) && is_array($_POST['hobbies'])) {
+        $interests = $_POST['hobbies'];
+        echo "You selected the following interests: ";
+        foreach ($interests as $interest) {
+            echo htmlspecialchars($interest) . " ";
+        }
+        echo "<br>";
+    } else {
+        echo "You didn't select any of 5 hobbies.<br>";
+    }
+}
+?>
+
+<form action="index.php" method="post">
+  <button type="submit">Reset Creation</button>
+</form>
+
+</body>
+</html>
