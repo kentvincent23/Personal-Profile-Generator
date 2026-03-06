4c598afb6bf8c7393d29aa49dc39ed558653dceb
kentvincent23 <k.agot.145976.tc@umindanao.edu.ph>
2026-03-06 14:11:33 +0800

    test

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang = 'en'>
+<title>Profile Generator Website</title>
+<head> 
+    <meta charset = 'UTF-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <form action="profile.php">
+        <label for="fname">Full Name:</label>
+        <input type="text" id="fname" name="fname"> <br><br>
+        <label for="age">Age:</label>
+        <input type="number" id="age" name="age"> <br><br>
+        <label for="course">Course/Program:</label>
+        <input type="text" id="course" name="course"> <br><br>
+        <label for="mail">E-mail Address:</label>
+        <input type="email" id="mail" name="mail"> <br><br>
+        
+        <label for="gender">Gender:</label>
+        <input type="radio" id="gender" name="gender" value="Male">
+        <label for="male"> Male </label>
+        <input type="radio" id="gender" name="gender" value="Feale">
+        <label for="female"> Female </label>
+
+
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<html lang = 'en'>
+<title>Profile Generator Website</title>
+<body> Hello </body>
